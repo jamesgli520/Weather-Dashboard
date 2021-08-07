cac17975dcb5859911141d064ea4bb697c09d62e
James Li <jgdli0701@gmail.com>
2021-08-06 20:19:10 -0400

    updated

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Weather-Dashboard
+
+**Version 1.0.0**
+
+## Description
+
+This is a javascript weather dashboard application that it shows the current weather condition of Atlanta.<br>
+And, user also can type the city name, then the current weather condition of the city and the five-day<br>
+forecast. There has several weather data shows on the screen which they are Temperature in censius degree, <br>
+Humidity, Wind-Speed, and UV Index.<br>
+Which the UV Index will shows green, yellow, orange and red color indicates whether the condition is low, moderate,<br>
+High, and severe.<br>
+Each city that user searched will be saved, and they can view the weather of each city just by click on the city name.<br>
+
+<p>
+    <img src="" /></br></br>
+    
+</p>
+
+
+## Link to Deployed Application
+
+Live: 
+Github repository: 
+
+## License & copyright
+
+## Made By James
+
+&copy; 2021.
--- js/script.js
+++ js/script.js
@@ -22,7 +22,20 @@ var latitude, longtitude;
         pElTemATL.innerHTML = 'Temperture: '+data.current.temp +'&deg;C';
         pElHumATL.innerHTML = 'Humidity: '+data.current.humidity +'%';
         pElWindATL.innerHTML = 'Wind Speed: '+data.current.wind_speed +' MPH';
-        pElUVIndATL.innerHTML = 'UV index: '+data.current.uvi;
+        
+        if(data.current.uvi < 2){
+          pElUVIndATL.innerHTML = 'UV index: '+data.current.uvi;
+          pElUVIndATL.setAttribute('style','background-color:green');
+        }else if(data.current.uvi > 2 || data.current.uvi <=5){
+          pElUVIndATL.innerHTML = 'UV index: '+data.current.uvi;
+          pElUVIndATL.setAttribute('style','background-color:yellow');
+        }else if(data.current.uvi > 5 || data.current.uvi <=7){
+          pElUVIndATL.innerHTML = 'UV index: '+data.current.uvi;
+          pElUVIndATL.setAttribute('style','background-color:orange');
+        }else{
+          pElUVIndATL.innerHTML = 'UV index: '+data.current.uvi;
+          pElUVIndATL.setAttribute('style','background-color:red');
+        }
 
     });
 
@@ -43,8 +56,9 @@ function searchLatLon(){
           if(latitude !== 'null' && longtitude !== 'null'){
             //atlantaWeather();
             cityWeatherData();
-
-
+            localStorage.setItem(inputCityName.value, inputCityName.value)
+            var label1El = document.querySelector('.label1');
+            label1El.innerHTML = localStorage.getItem(inputCityName.value);
           }
 
       });
@@ -115,33 +129,110 @@ function cityWeatherData(){
           pElTemC.innerHTML = 'Temperture: '+data.current.temp +'&deg;C';
           pElHumC.innerHTML = 'Humidity: '+data.current.humidity +'%';
           pElWindC.innerHTML = 'Wind Speed: '+data.current.wind_speed +' MPH';
-          pElUVIndc.innerHTML = 'UV index: '+data.current.uvi;
+          if(data.current.uvi < 2){
+            pElUVIndc.innerHTML = 'UV index Low: '+data.current.uvi;
+            pElUVIndc.setAttribute('style','background-color:green');
+          }else if(data.current.uvi > 2 || data.current.uvi <=5){
+            pElUVIndc.innerHTML = 'UV index Moderate: '+data.current.uvi;
+            pElUVIndc.setAttribute('style','background-color:yellow');
+          }else if(data.current.uvi > 5 || data.current.uvi <=7){
+            pElUVIndc.innerHTML = 'UV index High: '+data.current.uvi
+            pElUVIndc.setAttribute('style','background-color:orange');
+          }else{
+            pElUVIndc.innerHTML = 'UV index Severe: '+data.current.uvi;
+            pElUVIndc.setAttribute('style','background-color:red');
+          }
 
+          h3ElDateDate1.innerHTML = (new Date().getFullYear())+'-'+(new Date().getMonth()+1) +'-'+ (new Date().getDate()+1);
           pElTem1.innerHTML = "<img src='http://openweathermap.org/img/wn/"+data.daily[0].weather[0].icon+".png'>"+'Temperture: '+data.daily[0].temp.day+'&deg;C';
           pElHum1.innerHTML = 'Humidity: '+data.daily[0].humidity+'%';
           pElWind1.innerHTML = 'Wind Speed: '+data.daily[0].wind_speed+' MPH';
-          pElUVInd1.innerHTML = 'UV index: '+data.daily[0].uvi;
+          if(data.daily[0].uvi < 2){
+            pElUVInd1.innerHTML = 'UV index Low: '+data.daily[0].uvi;
+            pElUVInd1.setAttribute('style','background-color:green');
+          }else if(data.daily[0].wind_speed > 2 || data.current.uvi <=5){
+            pElUVInd1.innerHTML = 'UV index Moderate: '+data.daily[0].uvi;
+            pElUVInd1.setAttribute('style','background-color:yellow');
+          }else if(data.daily[0].wind_speed > 5 || data.current.uvi <=7){
+            pElUVInd1.innerHTML = 'UV index High: '+data.daily[0].uvi;
+            pElUVInd1.setAttribute('style','background-color:orange');
+          }else{
+            pElUVInd1.innerHTML = 'UV index Severe: '+data.daily[0].uvi;
+            pElUVInd1.setAttribute('style','background-color:red');
+          }
 
+          h3ElDateDate2.innerHTML = (new Date().getFullYear())+'-'+(new Date().getMonth()+1) +'-'+ (new Date().getDate()+2);
           pElTem2.innerHTML = "<img src='http://openweathermap.org/img/wn/"+data.daily[1].weather[0].icon+".png'>"+'Temperture: '+data.daily[1].temp.day +'&deg;C';
           pElHum2.innerHTML = 'Humidity: '+data.daily[1].humidity+'%';
           pElWind2.innerHTML = 'Wind Speed: '+data.daily[1].wind_speed +' MPH';
-          pElUVInd2.innerHTML = 'UV index: '+data.daily[1].uvi;
+          if(data.daily[1].uvi < 2){
+            pElUVInd2.innerHTML = 'UV index Low: '+data.daily[1].uvi;
+            pElUVInd2.setAttribute('style','background-color:green');
+          }else if(data.daily[1].uvi > 2 || data.daily[1].uvi <=5){
+            pElUVInd2.innerHTML = 'UV index Moderate: '+data.daily[1].uvi;
+            pElUVInd2.setAttribute('style','background-color:yellow');
+          }else if(data.daily[1].uvi > 5 || data.daily[1].uvi <=7){
+            pElUVInd2.innerHTML = 'UV index High: '+data.daily[1].uvi;
+            pElUVInd2.setAttribute('style','background-color:orange');
+          }else{
+            pElUVInd2.innerHTML = 'UV index Severe: '+data.daily[1].uvi;
+            pElUVInd2.setAttribute('style','background-color:red');
+          }
 
+          h3ElDateDate3.innerHTML = (new Date().getFullYear())+'-'+(new Date().getMonth()+1) +'-'+ (new Date().getDate()+3);
           pElTem3.innerHTML = "<img src='http://openweathermap.org/img/wn/"+data.daily[2].weather[0].icon+".png'>"+'Temperture: '+data.daily[2].temp.day +'&deg;C';
           pElHum3.innerHTML = 'Humidity: '+data.daily[2].humidity+'%';
           pElWind3.innerHTML = 'Wind Speed: '+data.daily[2].wind_speed;
-          pElUVInd3.innerHTML = 'UV index: '+data.daily[2].uvi;
+          if(data.daily[2] < 2){
+            pElUVInd3.innerHTML = 'UV index Low: '+data.daily[2].uvi;
+            pElUVInd3.setAttribute('style','background-color:green');
+          }else if(data.daily[2] > 2 || data.daily[2].uvi <=5){
+            pElUVInd3.innerHTML = 'UV index Moderate: '+data.daily[2].uvi;
+            pElUVInd3.setAttribute('style','background-color:yellow');
+          }else if(data.daily[2] > 5 || data.daily[2].uvi <=7){
+            pElUVInd3.innerHTML = 'UV index High: '+data.daily[2].uvi;
+            pElUVInd3.setAttribute('style','background-color:orange');
+          }else{
+            pElUVInd3.innerHTML = 'UV index Severe: '+data.daily[2].uvi;
+            pElUVInd3.setAttribute('style','background-color:red');
+          }
 
+          h3ElDateDate4.innerHTML = (new Date().getFullYear())+'-'+(new Date().getMonth()+1) +'-'+ (new Date().getDate()+4);
           pElTem4.innerHTML = "<img src='http://openweathermap.org/img/wn/"+data.daily[3].weather[0].icon+".png'>"+'Temperture: '+data.daily[3].temp.day +'&deg;C';
           pElHum4.innerHTML = 'Humidity: '+data.daily[3].humidity+'%';
           pElWind4.innerHTML = 'Wind Speed: '+data.daily[3].wind_speed+' MPH';
           pElUVInd4.innerHTML = 'UV index: '+data.daily[3].uvi;
+          if(data.daily[3].uvi < 2){
+            pElUVInd4.innerHTML = 'UV index Low: '+data.daily[3].uvi;
+            pElUVInd4.setAttribute('style','background-color:green');
+          }else if(data.daily[3].uvi > 2 || data.current.uvi <=5){
+            pElUVInd4.innerHTML = 'UV index Moderate: '+data.daily[3].uvi;
+            pElUVInd4.setAttribute('style','background-color:yellow');
+          }else if(data.daily[3].uvi > 5 || data.current.uvi <=7){
+            pElUVInd4.innerHTML = 'UV index High: '+data.daily[3].uvi;
+            pElUVInd4.setAttribute('style','background-color:orange');
+          }else{
+            pElUVInd4.innerHTML = 'UV index Severe: '+data.daily[3].uvi;
+            pElUVInd4.setAttribute('style','background-color:red');
+          }
 
+          h3ElDateDate5.innerHTML = (new Date().getFullYear())+'-'+(new Date().getMonth()+1) +'-'+ (new Date().getDate()+5);
           pElTem5.innerHTML = "<img src='http://openweathermap.org/img/wn/"+data.daily[4].weather[0].icon+".png'>"+'Temperture: '+data.daily[4].temp.day +'&deg;C';
           pElHum5.innerHTML = 'Humidity: '+data.daily[4].humidity+'%';
           pElWind5.innerHTML = 'Wind Speed: '+data.daily[4].wind_speed+' MPH';
-          pElUVInd5.innerHTML = 'UV index: '+data.daily[4].uvi;
-
+          if(data.daily[4].uvi < 2){
+            pElUVInd5.innerHTML = 'UV index Low: '+data.daily[4].uvi;
+            pElUVInd5.setAttribute('style','background-color:green');
+          }else if(data.daily[4].uvi > 2 || data.current.uvi <=5){
+            pElUVInd5.innerHTML = 'UV index Moderate: '+data.daily[4].uvi;
+            pElUVInd5.setAttribute('style','background-color:yellow');
+          }else if(data.daily[4].uvi > 5 || data.current.uvi <=7){
+            pElUVInd5.innerHTML = 'UV index High: '+data.daily[4].uvi;
+            pElUVInd5.setAttribute('style','background-color:orange');
+          }else{
+            pElUVInd5.innerHTML = 'UV index Severe: '+data.daily[4].uvi;
+            pElUVInd5.setAttribute('style','background-color:red');
+          }
           
       });
       
